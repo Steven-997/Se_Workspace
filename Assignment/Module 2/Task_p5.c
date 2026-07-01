cae9a48f175e29a30b014aeb951ac9f884a1df69
Steven-997 <stevennndesai@gmail.com>
2026-07-01 12:15:52 +0530

    commit

---
+++ Assignment/Module 2/Task_p5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() 
+{
+    float base = 10;
+    int height = 5;
+    int area = 0.5 * base * height;
+
+    printf("\nArea of the triangle = %d\n", area);
+
+    getch();
+}
